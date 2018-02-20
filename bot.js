ecc2af6c9c75ca7b9e56a85c174603521ffe0066
Simon Clarke <simon@zeak.co>
2018-02-20 16:51:41 +0000

    This might fix the heroku boot timeout

--- bot.js
+++ bot.js
@@ -25,3 +25,12 @@ client.on("message", msg => {
 });
 
 client.login(token);
+
+var port = process.env.PORT || 5000;
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(port);
