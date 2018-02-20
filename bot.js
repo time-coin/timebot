1da62856202dadff93df2e8f34a6d272edccc04c
Simon Clarke <simon@zeak.co>
2018-02-20 16:05:18 +0000

    Test start bot

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const Commando = require("discord.js-commando");
+const client = new Commando.Client({ owner: process.env.DISCORD_BOT_OWNER });
+const token = process.env.DISCORD_API_KEY;
+const path = require("path");
+
+client.registry
+  // Registers your custom command groups
+  .registerGroups([["base", "Auction"]])
+
+  // Registers all built-in groups, commands, and argument types
+  .registerDefaults()
+
+  // Registers all of your commands in the ./commands/ directory
+  .registerCommandsIn(path.join(__dirname, "commands"));
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!");
+  }
+});
+
+client.login(token);
