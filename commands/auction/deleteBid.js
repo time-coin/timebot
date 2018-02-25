6000f58f980d530de3083895aee655afa74114d0
Simon Clarke <simon@zeak.co>
2018-02-25 15:00:44 +0000

    yay more bugs

--- commands/auction/deleteBid.js
+++ commands/auction/deleteBid.js
@@ -28,7 +28,8 @@ module.exports = class deleteAuction extends Commando.Command {
     }
     redis.set(redis.getKeyFromMsg(msg, "active"), true);
     let state = await auction.getState(msg);
-    if (state.bids.length > 2) {
+    console.log(JSON.stringify(state));
+    if (state.bids.length >= 2) {
       let old_high_bid = state.bids.pop();
       let new_last_bid = state.bids.pop();
       state.bids = [...state.bids, new_last_bid];
