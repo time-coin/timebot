9835f349520c39beedb3e3d8db09f86b5421c5f8
Simon Clarke <simon@zeak.co>
2018-02-25 13:23:18 +0000

    Deletebid handle no more last bids

--- commands/auction/deleteBid.js
+++ commands/auction/deleteBid.js
@@ -28,17 +28,22 @@ module.exports = class deleteAuction extends Commando.Command {
     }
     redis.set(redis.getKeyFromMsg(msg, "active"), true);
     let state = await auction.getState(msg);
-    let old_high_bid = state.bids.pop();
-    let new_last_bid = state.bids.pop();
-    state.bids = [...state.bids, new_last_bid];
-    state.amount = new_last_bid.amount;
-    auction.setState(msg, state);
-    return msg.reply(
-      "The last high bid has been removed. The new high bid is ** " +
-        new_last_bid.amount.toFixed(3) +
-        " BTC**  by **" +
-        new_last_bid.user +
-        "**"
-    );
+    if (state.bids.length) {
+      let old_high_bid = state.bids.pop();
+      let new_last_bid = state.bids.pop();
+      state.bids = [...state.bids, new_last_bid];
+      state.amount = new_last_bid.amount;
+      auction.setState(msg, state);
+      return msg.reply(
+        "The last high bid has been removed. The new high bid is ** " +
+          new_last_bid.amount.toFixed(3) +
+          " BTC**  by **" +
+          new_last_bid.user +
+          "**"
+      );
+    } else {
+      auction.setState(msg, auction.defaultState);
+      msg.reply("The kast bid has been removed.");
+    }
   }
 };
