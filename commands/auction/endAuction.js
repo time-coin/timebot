ce63e26df6bce55ff31c99356fb921e74901ec5e
Simon Clarke <loony383@users.noreply.github.com>
2018-02-25 13:02:09 +0000

    Update endAuction.js

--- commands/auction/endAuction.js
+++ commands/auction/endAuction.js
@@ -24,8 +24,10 @@ module.exports = class cancelAuction extends Commando.Command {
     if (!auction.isAuctionChannel(msg)) {
       return false;
     }
+    let state = await auction.getState(msg);
     redis.delete(redis.getKeyFromMsg(msg, "auction"));
+    let winner = state.bids.pop();
     redis.set(redis.getKeyFromMsg(msg, "active"), false);
-    return msg.reply(`Auction ended.`);
+    return msg.reply("Auction ended. The winner is **" + winner.user + "** at **" + winner.amount + "**");
   }
 };
