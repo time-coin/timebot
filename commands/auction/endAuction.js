2d41a09399330e05d0308cc0ab4d5404fd7621d3
Simon Clarke <loony383@users.noreply.github.com>
2018-02-25 13:03:31 +0000

    Only end an auction if there is one.

--- commands/auction/endAuction.js
+++ commands/auction/endAuction.js
@@ -24,10 +24,15 @@ module.exports = class cancelAuction extends Commando.Command {
     if (!auction.isAuctionChannel(msg)) {
       return false;
     }
-    let state = await auction.getState(msg);
-    redis.delete(redis.getKeyFromMsg(msg, "auction"));
-    let winner = state.bids.pop();
-    redis.set(redis.getKeyFromMsg(msg, "active"), false);
-    return msg.reply("Auction ended. The winner is **" + winner.user + "** at **" + winner.amount + "**");
+    const isActive = await auction.isAuctionActive(msg);
+    if (!isActive) {
+      msg.reply("There is currently no active auction.");
+    } else {
+        let state = await auction.getState(msg);
+        redis.delete(redis.getKeyFromMsg(msg, "auction"));
+        let winner = state.bids.pop();
+        redis.set(redis.getKeyFromMsg(msg, "active"), false);
+        return msg.reply("Auction ended. The winner is **" + winner.user + "** at **" + winner.amount + "**");
+    }
   }
 };
